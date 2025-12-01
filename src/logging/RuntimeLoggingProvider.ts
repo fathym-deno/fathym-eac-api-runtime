dd567155c2fe01d0d017ac932203a688893c2551
mcgear <michael.gearhardt@gmail.com>
2025-12-01 13:31:24 -0700

    formatting

--- src/logging/RuntimeLoggingProvider.ts
+++ src/logging/RuntimeLoggingProvider.ts
@@ -1,10 +1,10 @@
-import { EaCApplicationsLoggingProvider } from '@fathym/eac-applications/runtime/logging';
+import { EaCApplicationsLoggingProvider } from "@fathym/eac-applications/runtime/logging";
 
 export class RuntimeLoggingProvider extends EaCApplicationsLoggingProvider {
   constructor() {
     const loggingPackages = [
-      '@fathym/common',
-      '@fathym/fathym-eac-api-runtime',
+      "@fathym/common",
+      "@fathym/fathym-eac-api-runtime",
     ];
 
     super(loggingPackages);
--- src/plugins/RuntimePlugin.ts
+++ src/plugins/RuntimePlugin.ts
@@ -1,8 +1,14 @@
-import { EaCRuntimeConfig, EaCRuntimePluginConfig } from '@fathym/eac/runtime/config';
-import { EaCRuntimePlugin } from '@fathym/eac/runtime/plugins';
-import { EverythingAsCode } from '@fathym/eac';
-import { EverythingAsCodeApplications } from '@fathym/eac-applications';
-import { EaCStewardAPIPlugin, EaCStewardPlugin } from '@fathym/eac-applications/steward/plugins';
+import {
+  EaCRuntimeConfig,
+  EaCRuntimePluginConfig,
+} from "@fathym/eac/runtime/config";
+import { EaCRuntimePlugin } from "@fathym/eac/runtime/plugins";
+import { EverythingAsCode } from "@fathym/eac";
+import { EverythingAsCodeApplications } from "@fathym/eac-applications";
+import {
+  EaCStewardAPIPlugin,
+  EaCStewardPlugin,
+} from "@fathym/eac-applications/steward/plugins";
 
 export default class RuntimePlugin implements EaCRuntimePlugin {
   constructor() {}
@@ -17,29 +23,29 @@ export default class RuntimePlugin implements EaCRuntimePlugin {
         Projects: {
           core: {
             Details: {
-              Name: 'Fathy EaC Steward API Runtime',
+              Name: "Fathy EaC Steward API Runtime",
               Description:
-                'This runtime provides the EaC Steward APIs for interacting with the EaC Steward system (commits, status and more).',
+                "This runtime provides the EaC Steward APIs for interacting with the EaC Steward system (commits, status and more).",
               Priority: 100,
             },
             ResolverConfigs: {
               localhost: {
-                Hostname: 'localhost',
+                Hostname: "localhost",
                 Port: config.Servers![0].port || 8000,
               },
-              '127.0.0.1': {
-                Hostname: '127.0.0.1',
+              "127.0.0.1": {
+                Hostname: "127.0.0.1",
                 Port: config.Servers![0].port || 8000,
               },
-              'host.docker.internal': {
-                Hostname: 'host.docker.internal',
+              "host.docker.internal": {
+                Hostname: "host.docker.internal",
                 Port: config.Servers![0].port || 8000,
               },
-              'eac-steward.fathym.com': {
-                Hostname: 'eac-steward.fathym.com',
+              "eac-steward.fathym.com": {
+                Hostname: "eac-steward.fathym.com",
               },
-              'fathym-eac-api-runtime.azurewebsites.net': {
-                Hostname: 'fathym-eac-api-runtime.azurewebsites.net',
+              "fathym-eac-api-runtime.azurewebsites.net": {
+                Hostname: "fathym-eac-api-runtime.azurewebsites.net",
               },
             },
             ModifierResolvers: {},
